7482ff42f0bb9ff85e437b4fc7fa4ad7f5104234
JGrain2 <77919586+JGrain2@users.noreply.github.com>
2024-06-06 14:37:03 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py - ISE server details and credentials
+
+# ISE server details and credentials
+ISE_SERVER = 'https://<ISE SERVER>:9060'
+USERNAME = 'admin'
+PASSWORD = 'password'
+
+# Device details
+DEVICE_IP = ''
+DEVICE_IP_MASK = 32
+RADIUS_KEY = 'key'
+DEVICE_NAME = ""
+DESCRIPTION = 'Network device added via API'
+
+# Device credentials
+DEVICE_USERNAME = 'user'
+DEVICE_PASSWORD = 'password'
+DEVICE_SECRET = 'password'
